0a204eec683c8aaa4aadb03e8e7e1f0345216bf7
Lasya <139505288+lasyaramakrishnan@users.noreply.github.com>
2023-07-18 17:03:23 -0700

    Update lab05.py

--- labs/lab05.py
+++ labs/lab05.py
@@ -1,1 +1,195 @@
+def if_this_not_that(i_list, this):
+    """Define a function which takes a list of integers `i_list` and an integer `this`.
+    For each element in `i_list`, print the element if it is larger than `this`;
+    otherwise, print the word "that".
+    >>> original_list = [1, 2, 3, 4, 5]
+    >>> if_this_not_that(original_list, 3)
+    that
+    that
+    that
+    4
+    5
+    """
+    "*** YOUR CODE HERE ***"
+    for x in i_list: 
+    	if x > this: 
+    		print (x)
+    	else:
+    		print ("that")
 
+def couple(s1, s2):
+    """Return a list that contains lists with i-th elements of two sequences
+    coupled together.
+    >>> s1 = [1, 2, 3]
+    >>> s2 = [4, 5, 6]
+    >>> couple(s1, s2)
+    [[1, 4], [2, 5], [3, 6]]
+    >>> s3 = ['c', 6]
+    >>> s4 = ['s', '1']
+    >>> couple(s3, s4)
+    [['c', 's'], [6, '1']]
+    """
+    assert len(s1) == len(s2)
+
+    "*** YOUR CODE HERE ***"
+    return [[s1[i], s2[i]] for i in range(len(s1))]
+
+
+def enumerate(s, start=0):
+    """Returns a list of lists, where the i-th list contains i+start and
+    the i-th element of s.
+    >>> enumerate([6, 1, 'a'])
+    [[0, 6], [1, 1], [2, 'a']]
+    >>> enumerate('five', 5)
+    [[5, 'f'], [6, 'i'], [7, 'v'], [8, 'e']]
+    """
+    end = []
+    i = start
+    for item in s:
+        end += [[i, item]]
+        i += 1
+    return end
+
+def squares(s):
+    """Returns a new list containing square roots of the elements of the original list
+    that are perfect squares.
+    >>> seq = [8, 49, 8, 9, 2, 1, 100, 102]
+    >>> squares(seq)
+    [7, 3, 1, 10]
+    >>> seq = [500, 30]
+    >>> squares(seq)
+    []
+    """
+    from math import sqrt
+    return [int(sqrt(x)) for x in s if int(sqrt(x)) ** 2 == x]
+
+def key_of_min_value(d):
+    """Returns the key in a dict d that corresponds to the minimum value of d.
+    >>> letters = {'a': 6, 'b': 5, 'c': 4, 'd': 5}
+    >>> min(letters)
+    'a'
+    >>> key_of_min_value(letters)
+    'c'
+    """
+    return min(d, key=lambda x: d[x])
+
+def flatten(lst):
+    """Returns a flattened version of lst.
+    >>> flatten([1, 2, 3])     # normal list
+    [1, 2, 3]
+    >>> x = [1, [2, 3], 4]      # deep list
+    >>> flatten(x)
+    [1, 2, 3, 4]
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1]
+    """
+    flattened = []
+    for item in lst:
+        if type(item) == list:
+            flattened += flatten(item)
+        else:
+            flattened = flattened + [item]
+    return flattened
+
+def add_chars(w1, w2):
+    """
+    Return a string containing the characters you need to add to w1 to get w2.
+    You may assume that w1 is a subsequence of w2.
+
+    >>> add_chars("owl", "howl")
+    'h'
+    >>> add_chars("want", "wanton")
+    'on'
+    >>> add_chars("rat", "radiate")
+    'diae'
+    >>> add_chars("a", "prepare")
+    'prepre'
+    >>> add_chars("resin", "recursion")
+    'curo'
+    >>> add_chars("fin", "effusion")
+    'efuso'
+    >>> add_chars("coy", "cacophony")
+    'acphon'
+    """
+    "*** YOUR CODE HERE ***"
+
+    if len(w1) == 0:  
+        return w2
+    if len(w2) == 0:  
+        return ''
+    elif w1[0] == w2[0]:  
+        return add_chars(w1[1:], w2[1:])
+    else:  
+        return w2[0] + add_chars(w1, w2[1:])
+
+def make_city(name, lat, lon):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_name(city)
+    'Berkeley'
+    >>> get_lat(city)
+    0
+    >>> get_lon(city)
+    1
+    """
+    return [name, lat, lon]
+
+def get_name(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_name(city)
+    'Berkeley'
+    """
+    return city[0]
+
+def get_lat(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_lat(city)
+    0
+    """
+    return city[1]
+
+def get_lon(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_lon(city)
+    1
+    """
+    return city[2]
+
+from math import sqrt
+def distance(city1, city2):
+    """
+    >>> city1 = make_city('city1', 0, 1)
+    >>> city2 = make_city('city2', 0, 2)
+    >>> distance(city1, city2)
+    1.0
+    >>> city3 = make_city('city3', 6.5, 12)
+    >>> city4 = make_city('city4', 2.5, 15)
+    >>> distance(city3, city4)
+    5.0
+    """
+    return sqrt(((get_lat(city1) - get_lat(city2))**2 + ((get_lon(city1)) - get_lon(city2))**2))
+
+
+def closer_city(lat, lon, city1, city2):
+    """
+    Returns the name of either city1 or city2, whichever is closest to
+    coordinate (lat, lon).
+
+    >>> berkeley = make_city('Berkeley', 37.87, 112.26)
+    >>> stanford = make_city('Stanford', 34.05, 118.25)
+    >>> closer_city(38.33, 121.44, berkeley, stanford)
+    'Stanford'
+    >>> bucharest = make_city('Bucharest', 44.43, 26.10)
+    >>> vienna = make_city('Vienna', 48.20, 16.37)
+    >>> closer_city(41.29, 174.78, bucharest, vienna)
+    'Bucharest'
+    """
+
+    silly_city = make_city('silly', lat, lon)
+    if (distance(silly_city, city1) >= distance(silly_city, city2)):
+        return get_name(city2)
+    return get_name(city1)
